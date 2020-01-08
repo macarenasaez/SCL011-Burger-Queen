7ed97f46e689a9ac31f840499308082375e0ca6b
Crusiris Nuñez <45858515+Crusiris@users.noreply.github.com>
2020-01-08 17:07:06 -0300

    Creando el deploy con firebase (#18)
    Deploy con firebase

--- my-app/src/environments/environment.prod.ts
+++ my-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDl_0NPCp5xmqcVz_nKH2tqC4yg1c-ck2k",
+    authDomain: "burguer-queen-7d0ee.firebaseapp.com",
+    databaseURL: "https://burguer-queen-7d0ee.firebaseio.com",
+    projectId: "burguer-queen-7d0ee",
+    storageBucket: "burguer-queen-7d0ee.appspot.com",
+    messagingSenderId: "985843273352",
+    appId: "1:985843273352:web:bcf8ade3028721819f193d" 
+  }
 };
